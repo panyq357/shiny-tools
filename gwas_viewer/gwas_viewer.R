c82cf3146539afc48de0f0f47ba34e9516d030c8
panyq357 <panyq357@qq.com>
2023-10-13 23:29:30 +0800

    re-factor gwas_viewer.R

---
+++ gwas_viewer/gwas_viewer.R
@@ -0,0 +1,169 @@
+library(tidyverse)
+library(shiny)
+library(Gviz)
+
+
+## TODOs
+## - chr, peak plot brush y axis recognize
+## - chr, peak plot position connect with input frame
+## - snp info hover
+## - 
+
+source("gwas_viewer_prep.R")
+
+plot_manhattan = function(result_df) {
+    manhattan <- ggplot(result_df) +
+        geom_point(aes(x = pos, y = -log10(p))) +
+        facet_wrap(~chr, nrow = 1, scales = "free_x") +
+        labs(x = NULL, y = expression(-log[10](p.value))) +
+        scale_y_continuous(expand = expansion(mult = c(0, 0.2))) +
+        theme_bw() +
+        theme(panel.spacing.x = unit(0,"line"))
+    return(manhattan)
+}
+
+plot_global_manhattan = function(result_df) {
+    result_df |>
+        plot_manhattan() +
+        theme(axis.text.x=element_blank())
+}
+
+plot_chromosome_manhattan = function(result_df, chromosome) {
+    result_df |>
+        subset(chr == chromosome) |>
+        plot_manhattan() +
+        scale_x_continuous(breaks = 0:10 * 10 * 1e6, labels = paste0(0:10 * 10, "Mb"))
+}
+
+plot_peak_manhattan = function(result_df, chromosome, start, end) {
+    result_df |>
+        subset(chr == chromosome & pos >= start & pos <= end) |>
+        plot_manhattan()
+}
+
+
+
+plot_zoom_in_tracks = function(chromosome, start, end, result_df) {
+
+    result_df = subset(result_df, chr==chromosome & pos >= start & pos <= end)
+    gwas_gr = with(result_df, data.frame(chr, start=pos, end=pos, neg_log10_p=-log10(p))) |>
+        makeGRangesFromDataFrame(keep.extra.columns=TRUE)
+    manhattan = DataTrack(gwas_gr, name = "-log10(p.value)")
+
+    gene_region <- GeneRegionTrack(config$txdb)
+
+    axis = GenomeAxisTrack()
+
+    plotTracks(
+        list(manhattan, gene_region, axis),
+        transcriptAnnotation="transcript_id",
+        chromosome=chromosome,
+        from=start,
+        to=end
+    )
+}
+
+get_gene_annotation = function(chromosome, pos_start, pos_end) {
+
+    gene_gr <- genes(config$txdb)
+    gene_gr <- subset(gene_gr, start(gene_gr) > pos_start & end(gene_gr) < pos_end & seqnames(gene_gr) == chromosome)
+
+    gene_annotation = data.frame(
+        GeneID=gene_gr$gene_id,
+        Location=sprintf("%s:%s-%s(%s)", seqnames(gene_gr), start(gene_gr), end(gene_gr), strand(gene_gr))
+    )
+
+    gene_annotation = gene_annotation[order(gene_annotation$Location),]
+
+    gene_annotation = cbind(
+        gene_annotation, config$gene_anno[match(gene_annotation$GeneID, config$gene_anno[[1]]),-1]
+    )
+
+    return(gene_annotation)
+}
+
+
+### shiny app ###
+
+ui <- fluidPage(
+    titlePanel(config$title),
+    fluidRow(
+        column(width = 2,
+            selectInput("result_name", label = "Result", choices = names(config$result_list)),
+        ),
+        column(width = 2,
+            sliderInput("min_neg_log10_p_value", "Minimun -log10(p.value)", value = 2, min = 0, max = 10)
+        ),
+        column(width = 2,
+            downloadLink("downloadData", "Download Annotation")
+        )
+    ),
+    fluidRow(
+        column(width = 5,
+            plotOutput("global_manhattan", click = "global_manhattan_click")
+        ),
+        column(width = 3,
+            plotOutput("chromosome_manhattan", brush = "chromosome_manhattan_brush")
+        ),
+        column(width = 3,
+            plotOutput("peak_manhattan", brush = "peak_manhattan_brush")
+        ),
+    ),
+    fluidRow(
+        column(width = 11,
+            plotOutput("zoom_in_tracks")
+        )
+    ),
+    fluidRow(
+        column(width = 11,
+            dataTableOutput("gene_annotation")
+        )
+    )
+)
+
+server <- function(input, output, session) {
+    max_p_value <- reactive(10^(-input$min_neg_log10_p_value))
+
+    gwas_result <- reactive(config$result_list[[input$result_name]] |> subset(p <= max_p_value()))
+
+    output$global_manhattan <- renderPlot({
+        plot_global_manhattan(gwas_result())
+    })
+
+    chromosome <- reactive(input$global_manhattan_click$panelvar1)
+
+    output$chromosome_manhattan <- renderPlot({
+        plot_chromosome_manhattan(gwas_result(), chromosome())
+    })
+
+    chr_xmin <- reactive(input$chromosome_manhattan_brush$xmin)
+    chr_xmax <- reactive(input$chromosome_manhattan_brush$xmax)
+
+    output$peak_manhattan <- renderPlot({
+        plot_peak_manhattan(gwas_result(), chromosome(), chr_xmin(), chr_xmax())
+    })
+
+    peak_xmin <- reactive(as.integer(input$peak_manhattan_brush$xmin))
+    peak_xmax <- reactive(as.integer(input$peak_manhattan_brush$xmax))
+
+    output$zoom_in_tracks <- renderPlot({
+         plot_zoom_in_tracks(chromosome(), peak_xmin(), peak_xmax(), gwas_result())
+    })
+
+    output$gene_annotation <- renderDataTable({
+        get_gene_annotation(chromosome(), peak_xmin(), peak_xmax())
+    })
+
+    output$downloadData <- downloadHandler(
+        filename = function() {
+          paste("chromosome", chromosome(), "-", peak_xmin(), "-", peak_xmax(), ".csv", sep="")
+        },
+        content = function(file) {
+          write.csv(
+            get_gene_annotation(chromosome(), peak_xmin(), peak_xmax()), file)
+        }
+    )
+}
+
+shinyApp(ui, server)
+
---
+++ gwas_viewer/gwas_viewer_prep.R
@@ -0,0 +1,44 @@
+library(GenomicFeatures)
+
+# Prepare txdb for gene annotation track.
+gene_gr <- rtracklayer::import("/home/panyq/Tools/index-scripts/os/rap-db/2023-10-10/gtf/os.rap-db.make.gtf")
+seqlevels(gene_gr) <- sub("chr[0]*", "", seqlevels(gene_gr))
+txdb <- makeTxDbFromGRanges(gene_gr)
+
+# Prepare gene annotation data.frame.
+gene_anno <- readr::read_tsv("/home/panyq/Tools/index-scripts/os/rap-db/2023-10-10/rawdata/IRGSP-1.0_representative_annotation_2023-09-07.tsv.gz")
+gene_anno <- gene_anno[c("Locus_ID", "Description", "Oryzabase Gene Symbol Synonym(s)")]
+gene_anno <- gene_anno[!duplicated(gene_anno$Locus_ID),]
+
+# Prepare GWAS result data.frame list.
+result_list <- list(
+    "Blink_GL" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GL/GAPIT.Blink.GL.GWAS.Results.csv",
+    "CMLM_GL" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GL/GAPIT.CMLM.GL.GWAS.Results.csv",
+    "GLM_GL" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GL/GAPIT.GLM.GL.GWAS.Results.csv",
+    "Blink_GW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GW/GAPIT.Blink.GW.GWAS.Results.csv",
+    "CMLM_GW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GW/GAPIT.CMLM.GW.GWAS.Results.csv",
+    "GLM_GW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/GW/GAPIT.GLM.GW.GWAS.Results.csv",
+    "Blink_TGW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/TGW/GAPIT.Blink.TGW.GWAS.Results.csv",
+    "CMLM_TGW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/TGW/GAPIT.CMLM.TGW.GWAS.Results.csv",
+    "GLM_TGW" = "/mnt/d/Analysis/LYH.2022-04-01.3K-RG-2K-samples-GWAS/results/TGW/GAPIT.GLM.TGW.GWAS.Results.csv"
+) |> lapply(function(x) {
+    df <- readr::read_csv(x)
+    df <- df[c("Chromosome", "Position", "FDR_Adjusted_P-values")]
+    names(df) <- c("chr", "pos", "p")
+    return(df)
+})
+
+
+config = list(
+    title = "3K GWAS Viewer",
+
+    # A GRanges for Gviz to plot gene structure.
+    txdb = txdb,
+
+    # A data.frame, first column is gene_id, all other columns will be shown in shiny app.
+    gene_anno = gene_anno,
+
+    # A data.frame list, items are data.frames with three columns: "chr", "pos" and "p".
+    result_list = result_list
+)
+
